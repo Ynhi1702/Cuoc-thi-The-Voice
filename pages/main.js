bdd60d492017efc6db6176eaec9dd273f27a99c3
Ynhi1702 <sunhitran0109@gmail.com>
2025-08-06 15:31:33 +0700

    Add files via upload

--- pages/main.js
+++ pages/main.js
@@ -1,18 +1,34 @@
-// Countdown
+// main.js for The Voice Landing Page
 $(document).ready(function() {
-	$('#countdown').countdown('2026/08/06', function(event) {
-		$(this).html(
-			event.strftime('%D ngày %H:%M:%S đến thời điểm diễn ra sự kiện!')
-		);
-	});
+    // Đếm ngược đến ngày hết hạn đăng ký
+    $('#countdown-register').countdown('2026/07/20 23:59:59', function(event) {
+        if(event.offset.totalSeconds < 0) {
+            $(this).html("Đã hết hạn đăng ký!");
+        } else {
+            $(this).html(
+                event.strftime('%D ngày %H:%M:%S')
+            );
+        }
+    });
 
-	// Simple registration form interaction
-	$('#registerForm').submit(function(e) {
-		e.preventDefault();
-		$('#registerMessage').fadeIn();
-		setTimeout(() => {
-			$('#registerMessage').fadeOut();
-			$('#registerForm')[0].reset();
-		}, 3500);
-	});
+    // Đếm ngược đến ngày sự kiện
+    $('#countdown-event').countdown('2026/08/06 00:00:00', function(event) {
+        if(event.offset.totalSeconds < 0) {
+            $(this).html("Sự kiện đã bắt đầu!");
+        } else {
+            $(this).html(
+                event.strftime('%D ngày %H:%M:%S')
+            );
+        }
+    });
+
+    // Form đăng ký
+    $('#registerForm').submit(function(e) {
+        e.preventDefault();
+        $('#registerMessage').fadeIn();
+        setTimeout(() => {
+            $('#registerMessage').fadeOut();
+            $('#registerForm')[0].reset();
+        }, 3500);
+    });
 });
